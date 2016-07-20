c68dff558a2e6ea6cf5dfc9c3e9cd41849369622
Jargon <james@roguevector.com>
2016-07-21 00:15:19 +0100

    Fixed the array name variable in the wall helper.

--- src/helper.wall.js
+++ src/helper.wall.js
@@ -1,18 +1,18 @@
 var helperWall = {
     addWallEnds: function(wallArray, wallSide) {
-        var startX = exitAreas[exitAreaIndex][0].x;
-        var startY = exitAreas[exitAreaIndex][0].y;
-        var endX = exitAreas[exitAreaIndex][exitAreas[exitAreaIndex].length - 1].x;
-        var endY = exitAreas[exitAreaIndex][exitAreas[exitAreaIndex].length - 1].y;
+        var startX = wallArray[0].x;
+        var startY = wallArray[0].y;
+        var endX = wallArray[wallArray.length - 1].x;
+        var endY = wallArray[wallArray.length - 1].y;
 
     	switch (wallSide) {
     		case BOTTOM :
-                exitAreas[exitAreaIndex][exitAreas[exitAreaIndex].length] = { x: startX - 1, y: startY };
-                exitAreas[exitAreaIndex][exitAreas[exitAreaIndex].length] = { x: startX - 2, y: startY };
-                exitAreas[exitAreaIndex][exitAreas[exitAreaIndex].length] = { x: startX - 2, y: startY + 1 };
-                exitAreas[exitAreaIndex][exitAreas[exitAreaIndex].length] = { x: endX + 1, y: startY };
-                exitAreas[exitAreaIndex][exitAreas[exitAreaIndex].length] = { x: endX + 2, y: startY };
-                exitAreas[exitAreaIndex][exitAreas[exitAreaIndex].length] = { x: endX + 2, y: startY + 1 };
+                wallArray[wallArray.length] = { x: startX - 1, y: startY };
+                wallArray[wallArray.length] = { x: startX - 2, y: startY };
+                wallArray[wallArray.length] = { x: startX - 2, y: startY + 1 };
+                wallArray[wallArray.length] = { x: endX + 1, y: startY };
+                wallArray[wallArray.length] = { x: endX + 2, y: startY };
+                wallArray[wallArray.length] = { x: endX + 2, y: startY + 1 };
     			break;
     	}
     }
