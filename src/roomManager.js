a8a2c55826880fbb0f5b1f3dd78d0e0b1d53a95c
Jargon <james@roguevector.com>
2016-07-20 23:50:18 +0100

    Fixed temporary ends on bottom wall construction.

--- src/roomManager.js
+++ src/roomManager.js
@@ -117,6 +117,12 @@ var roomManager = {
             }
             lastX = bottomExitPoses[i].x;
         }
+        let startX = exitAreas[exitAreaIndex][0].x;
+        let startY = exitAreas[exitAreaIndex][0].y;
+        let endX = exitAreas[exitAreaIndex][exitAreas[exitAreaIndex].length - 1].x;
+        let endY = exitAreas[exitAreaIndex][exitAreas[exitAreaIndex].length - 1].y;
+        exitAreas[exitAreaIndex][exitAreas[exitAreaIndex].length] = { x: startX - 1, y: startY };
+        exitAreas[exitAreaIndex][exitAreas[exitAreaIndex].length] = { x: endX + 1, y: startY };
         exitAreaIndex++;
 
         lastX = undefined;
