c544e84b2f18ccc37e1f5cf5bc5a71932406dfd1
Jargon <james@roguevector.com>
2016-07-21 23:45:09 +0100

    Repair lowest percentage health first.

--- src/role.fixit.js
+++ src/role.fixit.js
@@ -35,6 +35,7 @@ var roleFixit = {
                         return (structure.hits < structure.hitsMax);
                     }
                 });
+                _.sortBy(structures, function(structure) { return structure.hits / structure.hitsMax; });
                 creep.memory.repairTargetId = structures[0].id;
             }
 
