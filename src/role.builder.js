e926538ac660c06f74996948dc5683a1a789eb3e
Jargon <james@roguevector.com>
2016-07-26 01:10:37 +0100

    Upgraded builders to grab energy from containers.

--- src/role.builder.js
+++ src/role.builder.js
@@ -3,8 +3,8 @@ var sourceHelper = require('helper.source');
 var roleBuilder = {
     /** @param {Creep} creep **/
     init: function(creep) {
-        creep.memory.targetIndex = creep.room.getUnderworkedSource();
-        sourceHelper.addWorker(creep.room.memory.sources[creep.memory.targetIndex]);
+        creep.memory.targetSourceIndex = undefined;
+        creep.memory.targetId = undefined;
         creep.memory.building = false;
         creep.memory.buildTargetId = undefined;
         console.log('[' + creep.name + '] Let\'s get building!');
@@ -13,10 +13,16 @@ var roleBuilder = {
     
     /** @param {Creep} creep **/
     cleanup: function(creepMemory, roomMemory) {
+        // TODO Remove the targetIndex stuff (left over from before).
         if (creepMemory.targetIndex !== undefined) {
             sourceHelper.removeWorker(roomMemory.sources[creepMemory.targetIndex]);
             creepMemory.targetIndex = undefined;
         }
+        if (creepMemory.targetSourceIndex !== undefined) {
+            sourceHelper.removeWorker(roomMemory.sources[creepMemory.targetSourceIndex]);
+            creepMemory.targetSourceIndex = undefined;
+        }
+        creepMemory.targetId = undefined;
         creepMemory.building = undefined;
         creepMemory.buildTargetId = undefined;
     },
@@ -50,10 +56,53 @@ var roleBuilder = {
                 return false;
             }
         } else {
-            if (creep.memory.targetIndex === undefined) return false; // Should have target, but if not let's cancel task.
-            var source = Game.getObjectById(creep.room.memory.sources[creep.memory.targetIndex].id);
-            if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source);
+            if (!creep.memory.targetId) {
+                if (creep.room.memory.containerCount) {
+
+                    let container = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                        filter: { structureType: STRUCTURE_CONTAINER }
+                    });
+
+                    if (container) {
+                        creep.memory.targetId = container.id;
+                    } else {
+                        console.log('Fallback');
+                        let containers = [];
+                        creep.room.memory.sources.forEach(function(source) {
+                            if (source.containerId) {
+                                containers[containers.length] = Game.getObjectById(source.containerId);
+                            }
+                        });
+
+                        if (containers.length) {
+                            containers = _.sortBy(containers, function(container) {
+                                return _.sum(container.store) * -1;
+                            });
+                            creep.memory.targetId = containers[0].id;
+                        }
+                    }
+                    
+                } else {
+                    creep.memory.targetSourceIndex = creep.room.getUnderworkedSource();
+                    creep.memory.targetId = creep.room.memory.sources[creep.memory.targetSourceIndex].id;
+                    sourceHelper.addWorker(creep.room.memory.sources[creep.memory.targetSourceIndex]);
+                }
+            }
+            if (creep.memory.targetId) {
+                if (creep.memory.targetSourceIndex) {
+                    let source = Game.getObjectById(creep.memory.targetId);
+                    if (creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(source);
+                    }
+                } else {
+                    let container = Game.getObjectById(creep.memory.targetId);
+                    let result = creep.withdraw(container, RESOURCE_ENERGY);
+                    if (result == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(container);
+                    } else if (result == ERR_NOT_ENOUGH_RESOURCES) {
+                        return false;
+                    }
+                }
             }
         }
         return true;
