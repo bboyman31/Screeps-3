9b1ee1711867febcb176243777ad981273e25b09
Jargon <james@roguevector.com>
2016-07-26 15:10:47 +0100

    Tower now attack, heal and repair (in that order).

--- src/manager.tower.js
+++ src/manager.tower.js
@@ -2,9 +2,30 @@ var towerManager = {
     run: function(room, towers) {
         let hostileCreeps = room.find(FIND_HOSTILE_CREEPS);
 
+        if (hostileCreeps.length === 0) {
+            var damagedCreeps = room.find(FIND_MY_CREEPS, {
+                filter: (creep) => {
+                    return (creep.hits < creep.hitsMax);
+                }
+            });
+        }
+
+        if (damagedCreeps.length === 0) {
+            var repairStructures = room.find(FIND_MY_STRUCTURES, {
+                filter: (structure) => {
+                    return (structure.hits < structure.hitsMax);
+                }
+            });
+            repairStructures = _.sortBy(structures, function(structure) { return structure.hits / Math.min(structure.hitsMax, 250000); });
+        }
+
         towers.forEach(function (tower) {
             if (hostileCreeps.length) {
                 tower.attack(hostileCreeps[0]);
+            } else if (damagedCreeps.length) {
+                tower.heal(damagedCreeps[0]);
+            } else if (repairStructures.length) {
+                tower.repair(repairStructures[0]);
             }
         });
     }
